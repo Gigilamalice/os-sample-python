d2c5dcde9942fe993ff23a41c904464a63022015
Gigilamalice <gigilamalice@gmail.com>
2019-11-19 16:53:54 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World forked! v3"
+    return "Hello World forked! v5"
 
 if __name__ == "__main__":
     application.run()
